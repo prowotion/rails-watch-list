dc4c7f37d00f6c42fd68bb39c80ccbf438268c79
guillaumeautret <guillaume.autret@gmail.com>
2024-11-14 14:52:42 +0100

    ça fonctionne

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,28 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+
+    if @bookmark.save
+      redirect_to lists_path
+    else
+      # raise
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :list_id, :comment)
+  end
 end
